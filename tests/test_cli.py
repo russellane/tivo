1cfda3c1d6b54f6b5a40ff40cbf47cdeba2aac56
Russel Lane <russel@rlane.com>
2022-05-26 14:04:04 -0500

    v1.0.2

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,6 +3,60 @@ import pytest
 from tivo.cli import main
 
 
+def test_version():
+    with pytest.raises(SystemExit) as err:
+        main(["--version"])
+    assert err.value.code == 0
+
+
+def test_help():
+    with pytest.raises(SystemExit) as err:
+        main(["--help"])
+    assert err.value.code == 0
+
+
+def test_md_help():
+    with pytest.raises(SystemExit) as err:
+        main(["--md-help"])
+    assert err.value.code == 0
+
+
+def test_long_help():
+    with pytest.raises(SystemExit) as err:
+        main(["--long-help"])
+    assert err.value.code == 0
+
+
+def test_bogus_option():
+    with pytest.raises(SystemExit) as err:
+        main(["--bogus-option"])
+    assert err.value.code == 2
+
+
+def test_bogus_argument():
+    with pytest.raises(SystemExit) as err:
+        main(["bogus-argument"])
+    assert err.value.code == 2
+
+
+def test_print_config():
+    with pytest.raises(SystemExit) as err:
+        main(["--print-config"])
+    assert err.value.code == 0
+
+
+def test_print_url():
+    with pytest.raises(SystemExit) as err:
+        main(["--print-url"])
+    assert err.value.code == 0
+
+
+def test_debug():
+    with pytest.raises(SystemExit) as err:
+        main(["-X"])
+    assert err.value.code == 0
+
+
 def test_list():
     main(["list"])
 
